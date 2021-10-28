cea64ec1d3b6bb81fa00ae1a53b427de53f43f2a
Matheus Torsoni de Campos <91149036+matheustorsoni@users.noreply.github.com>
2021-10-28 18:24:25 -0300

    creating global, hooks, providers, services

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React, { Profiler } from "react";
+import React from "react";
 import Layout from "./components/layout";
+import NoSearch from "./components/no-search";
+import Profile from "./components/profile";
+import Repositories from "./components/repositories";
+import useGithub from "./hooks/github-hooks";
 
-function App() {
+const App = () => {
+  const { githubState } = useGithub();
   return (
-     <main>
-    <Layout> <Profile />  </Layout>
-  </main>
+    <Layout>
+      {githubState.hasUser ? (
+        <>
+          {githubState.loading ? (
+            <p>Loading</p>
+          ) : (
+            <>
+              <Profile />
+              <Repositories />
+            </>
+          )}
+        </>
+      ) : (
+        <NoSearch />
+      )}
+    </Layout>
   );
-}   
+};
 
 export default App;
